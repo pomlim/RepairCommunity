6101a42413d4b8d27dfe75d22da699bef7585130
Meawhung <meawhung@gmail.com>
2023-01-22 18:29:15 +0900

    add default bg color

--- frontend-app/pages/_app.js
+++ frontend-app/pages/_app.js
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import '../components/modal/modal.css'
+import '../styles/globals.css';
+import '../components/modal/modal.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="bg-base-300">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
