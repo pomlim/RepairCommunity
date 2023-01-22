c081c8650fcb7d64ed34f2c4fcf996613b50fab0
Meawhung <meawhung@gmail.com>
2023-01-22 18:27:48 +0900

    add custom css

--- frontend-app/tailwind.config.js
+++ frontend-app/tailwind.config.js
@@ -5,15 +5,23 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      boxShadow: {
+        md: '0px 0px 8px rgba(159, 134, 25, 0.2)'
+      }
+    }
   },
   plugins: [require('daisyui')],
   daisyui: {
     themes: [
       {
         light: {
-          ...require('daisyui/src/colors/themes')['[data-theme=light]']
+          ...require('daisyui/src/colors/themes')['[data-theme=retro]'],
           // Customize light theme here
+          primary: '#C3C130',
+          'primary-content': '#443616',
+          'base-300': '#F9F3D9',
+          'neutral-content': '#FCFAED'
         }
       }
     ]
