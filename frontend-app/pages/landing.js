3bf4b8ce71346ba00f30e1a1827a769eb97ed2ee
meawhung <meawhung@gmail.com>
2023-06-06 14:27:38 +0000

    fix landing page link change to a href

--- frontend-app/pages/landing.js
+++ frontend-app/pages/landing.js
@@ -1,9 +1,10 @@
 import Image from 'next/image';
-import Link from 'next/link';
+// import Link from 'next/link';
 import Landing_img from '@/public/landing-img.png';
 import Reviv_logo from '@/public/Reviv_logo_landing.png';
 
 const LandingPage = ({}) => {
+  const url = `/shops`;
   return (
     <div className="w-full h-full p-4 bg-butter-default ">
       <div className="pt-20 p-4 text-center">
@@ -22,14 +23,14 @@ const LandingPage = ({}) => {
           <p>หรือใกล้ที่ทำงานก็หาได้ที่ Repair Market</p>
         </div>
         <div className="flex justify-center w-full pt-40 pb-8">
-          <Link href="/shops">
+          <a href={url}>
             <button
               type="button"
               className="h-12 text-base font-normal rounded-full w-80 btn btn-primary bg-green-default text-brown-default font-kanit"
             >
               เริ่มเลย
             </button>
-          </Link>
+          </a>
         </div>
       </div>
     </div>
